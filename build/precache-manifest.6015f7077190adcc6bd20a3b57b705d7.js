5720c73c951e28010f1583e241bc952c0da21654
taraschuiko <taraschuiko@gmail.com>
2018-12-18 19:44:12 +0200

    Added build

---
+++ build/precache-manifest.6015f7077190adcc6bd20a3b57b705d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44129fc9215ed2dc3711",
+    "url": "/static/js/main.44129fc9.chunk.js"
+  },
+  {
+    "revision": "0280caf922f405708f8c",
+    "url": "/static/js/1.0280caf9.chunk.js"
+  },
+  {
+    "revision": "44129fc9215ed2dc3711",
+    "url": "/static/css/main.c98c095f.chunk.css"
+  },
+  {
+    "revision": "c9c0fa73808e1a5117686daa9bf36f22",
+    "url": "/index.html"
+  }
+];
