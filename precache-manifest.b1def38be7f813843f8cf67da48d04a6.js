8c77fb9799ab4c253c88d4c711a7b5be76ca56d6
taraschuiko <taraschuiko@gmail.com>
2018-12-20 20:12:46 +0200

    Updates

---
+++ precache-manifest.b1def38be7f813843f8cf67da48d04a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06b812cd44f4156953d9",
+    "url": "/best-shop-ever/static/js/runtime~main.06b812cd.js"
+  },
+  {
+    "revision": "ca3211ee711852038535",
+    "url": "/best-shop-ever/static/js/main.ca3211ee.chunk.js"
+  },
+  {
+    "revision": "ca078c1b1e81827523ab",
+    "url": "/best-shop-ever/static/js/1.ca078c1b.chunk.js"
+  },
+  {
+    "revision": "ca3211ee711852038535",
+    "url": "/best-shop-ever/static/css/main.fde545f2.chunk.css"
+  },
+  {
+    "revision": "b89781332bd79c5b716035e0d4dddeee",
+    "url": "/best-shop-ever/index.html"
+  }
+];
